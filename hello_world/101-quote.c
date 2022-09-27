0112357e49aa5e67417c234fa33146c163cec8b8
AgustinMeriles <5720@holbertonstudents.com>
2022-09-27 08:25:14 -0700

    a

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 61);
+	return (1);
+}
