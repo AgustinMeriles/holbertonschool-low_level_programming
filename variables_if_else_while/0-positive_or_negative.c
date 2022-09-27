c056d70519156eafd6b296abafdf7819f673a222
AgustinMeriles <5720@holbertonstudents.com>
2022-09-27 09:46:51 -0700

    a

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-
 /**
  * function_name - Short description, single line
- * @n: Description of parameter x
+ * @void: Description of parameter x
  * Description: Longer description of the function)?
  * Return: 0
  */
-
-int main(nParameter)
+int main(void)
 {
-	int n = rand();
-	n = nParameter
-		return (n)
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf ("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf ("%d is negative\n", n);
+	}
+	else
+	{
+		printf ("%d is zero\n", n);
+	}
+	return (0);
 }
