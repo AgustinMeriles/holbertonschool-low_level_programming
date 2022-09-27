9161e2c3f70885482c6bb45d4b03fe81a774a472
AgustinMeriles <5720@holbertonstudents.com>
2022-09-27 07:32:11 -0700

    a

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return(0);
+	}
+
