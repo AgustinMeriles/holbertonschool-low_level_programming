b677078aeb8866ac80e3c2b886d038e8bae45c79
AgustinMeriles <5720@holbertonstudents.com>
2022-09-27 07:25:04 -0700

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function
+ * Return: 0
+ */
+
+
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle"); 
-	return (0); 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
