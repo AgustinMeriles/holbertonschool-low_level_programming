3e50c74696f226353cbc39a3dddc21ba041a7729
AgustinMeriles <5720@holbertonstudents.com>
2022-09-27 09:29:09 -0700

    a

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * function_name - Short description, single line
+ * @n: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+
+int main(nParameter)
+{
+	int n = rand();
+	n = nParameter
+		return (n)
+}
