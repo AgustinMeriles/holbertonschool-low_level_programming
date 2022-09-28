07365868e3da9cf431ad14b20e1589be639c4460
AgustinMeriles <5720@holbertonstudents.com>
2022-09-28 07:36:35 -0700

    a

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; ch++)
+putchar(abc);
+putchar('\n');
+return (0);
+}
