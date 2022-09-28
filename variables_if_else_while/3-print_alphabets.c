3bf835ae77922559320f309734c3a66bc77f28c3
AgustinMeriles <5720@holbertonstudents.com>
2022-09-28 07:41:55 -0700

    a

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)
+putchar(abc);
+putchar('\n');
+
+char ABC;
+for (ABC = 'A'; ABC <= 'Z'; ABC++)
+putchar(ABC)
+putchar('\n');
+return (0);
+}
