536c70958d1c87bf29b76e9448b2e2b580035789
AgustinMeriles <5720@holbertonstudents.com>
2022-09-28 08:39:28 -0700

    a

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 1; n < 10; n++)
+printf(n);
+return (0);
+}
