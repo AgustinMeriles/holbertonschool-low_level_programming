e97e70d3f09e1fff7327e768e72ce8831493a106
AgustinMeriles <5720@holbertonstudents.com>
2022-09-28 09:09:08 -0700

    a

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+putchar("\n");
+return (0);
+}
