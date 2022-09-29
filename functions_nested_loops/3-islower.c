c2431f8b9fc320c7c6226419a3eac08cc44b4ee5
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 08:43:33 -0700

    a

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <main.h>;
+/**
+ * function_name - Short description, single line
+ * @int c: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int _islower(int c)
+	{
+	char abc;
+	int num = 0;
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+	if (abc == 'c')
+		num = 1;
+	}
+	}
