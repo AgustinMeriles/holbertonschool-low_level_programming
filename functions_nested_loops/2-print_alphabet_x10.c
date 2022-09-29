2809d2d7e58e13363267b51b10304ee7580d09a5
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 07:33:22 -0700

    a

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ * @void: The character to print
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int repeats;
+int abc;
+for (repeats = 0; repeats < 10; repeats++)
+{
+for (abc = 'a'; abc < 'z'; abc++)
+_putchar(abc);
+_putchar('\n');
+}
+}
