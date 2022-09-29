90f52792b4720979a095c2a2692c377a2414e05c
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 09:09:05 -0700

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  * _islower - Check Holberton
+ *   * @c: An input character
+ *    * Description: function uses _putchar function to print
+ *     * alphabet in lowercase 10 times
+ *      * Return: 1 if is lowercase or 0 if is uppercase
+**/
+int _islower(int c)
+{
+char a, p;
+int num = 0;
+for (a = 'a'; a <= 'z'; a++)
+{
+for (p = 'A'; p <= 'z'; p++)
+{
+if (a == c || A == C)
+num = 1;
+}
+}
+return (num);
+}
