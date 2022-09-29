8b127d00b579d22e0667b69558793740f164b0bf
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 06:07:59 -0700

    a

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+
+int main(void)
+{
+	char abc;
+	for (abc = 'a'; abc <= 'z'; abc++)
+		puthcar(abc);
+		putchar('\n');
+	return (0);
+}
