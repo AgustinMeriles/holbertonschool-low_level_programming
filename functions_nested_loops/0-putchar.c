045fc6c5d2afc2027c9c26791832a286584de065
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 05:59:04 -0700

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -11,9 +11,9 @@
 int main(void)
 	{
 	char string[8] = "_putchar";
-	char pos;
+	int pos;
 		for (pos = 0; pos < 9; pos++)
-		putchar(string[pos])
-		putchar('\n')
+		putchar(string[pos]);
+		putchar('\n');
 	return (0);
 		}
