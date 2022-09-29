9e4bdc804eaab88350353c7dbeae2a2899b6fcdb
AgustinMeriles <5720@holbertonstudents.com>
2022-09-29 05:48:27 -0700

    a

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Short description, single line
+ * @void : Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+
+int main(void)
+	{
+	char string[8] = "_putchar";
+	printf(string);
+	return (0);
+		}
