a2c872ae072635fc1ea9eb3208afebbd928fbaeb
AgustinMeriles <5720@holbertonstudents.com>
2022-09-30 06:45:04 -0700

    a

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * print_last_digit - Short description, single line
+ * @n: parameter
+ * Description: Longer description of the function)?
+ * return: 0
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+printf(n);
+return (0);
+}
