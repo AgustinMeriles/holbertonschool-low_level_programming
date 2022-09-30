8e03471de1da9391e573f9de9d1467e3729d8799
AgustinMeriles <5720@holbertonstudents.com>
2022-09-30 10:33:24 -0700

    a

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Short description, single line
+ * @n, j: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: a + b 
+ */
+int add(int n, int j)
+{
+	return (n + j);
+}
