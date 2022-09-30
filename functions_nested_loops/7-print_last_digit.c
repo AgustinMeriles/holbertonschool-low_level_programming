9a6b84812027be75819171f098caa70b5c0132ff
AgustinMeriles <5720@holbertonstudents.com>
2022-09-30 09:08:09 -0700

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,15 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar('0' + n);
+	if (n >= 0)
+		{
+			n = n % 10;
+			_putchar('0' + n);
+		}
+	else 
+		{
+			n = (n * (-1)) % 10;
+			_putchar('0' + n);
+		}
 	return (n);
 }
