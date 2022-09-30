e0d5fb0fba94ccff0cf6dbbd23115cb01fce60eb
AgustinMeriles <5720@holbertonstudents.com>
2022-09-30 10:06:26 -0700

    a

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - Short description, single line
+ * @void: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+void times_table(void)
+	{
+		int num;
+		int table;
+	for (table = 0; table <= 9; num++)
+	{
+	for (num =  0; num <= 9; num++)
+	_putchar(table * num);
+	{
+	_putchar('\n')
+	}
+	}
+	return (0);
+	}
