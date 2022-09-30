893ebfdafd063a03efb5fd6441dea564ebb253b6
AgustinMeriles <5720@holbertonstudents.com>
2022-09-30 05:58:33 -0700

    a

---
+++ functions_nested_loops/5-sing.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - Short description, single line
+ * @n: Description of parameter x
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int print_sign(int n)
+{
+int value;
+if (n > 0)
+value = 1;
+_putchar ('+');
+else if (n == 0)
+value = 0;
+_putchar ('0');
+else
+value = -1;
+_putchar ('-');
+return (value);
+}
