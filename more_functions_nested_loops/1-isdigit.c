39c8bfca8e85f10890e40ea6d2632a76b946c019
AgustinMeriles <5720@holbertonstudents.com>
2022-10-04 06:28:22 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * _isdigit - Check Holberton
+ *   * @c: An input character
+ *    * Description: function that check if c is digit
+ *     * Return: 1 if "c" is digit or 0 if is otherwise
+ *      **/
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
