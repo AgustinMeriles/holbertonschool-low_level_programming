b8549b1da8495462a6df73fd20e348f6af9e5561
AgustinMeriles <5720@holbertonstudents.com>
2022-10-04 06:39:37 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Short description, single line
+ * @a: Description of parameter x
+ * @b: integrer
+ * Description: Longer description of the function)?
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (0);
+}
