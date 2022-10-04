cc0c727eefadc5a8b8fec425f28954963c4fe169
AgustinMeriles <5720@holbertonstudents.com>
2022-10-04 06:57:41 -0700

    a

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,10 @@
  * print_numbers - Short description, single line
  * @void: Description of parameter x
  * Description: Longer description of the function)?
- * Return: 0
  */
 void print_numbers(void)
 {
 for (int n = 0; n <= 9; n++)
 _putchar ('0' + n);
 _putchar ('\n');
-return (0);
 }
