047d20acb2cf2040fdf89006ed9c50b78d55ed42
AgustinMeriles <5720@holbertonstudents.com>
2022-10-05 05:57:20 -0700

    a

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *  * print_diagonal - Prints the diagonal depending to the input "n".
+ * *  * @n: input for the diagonal.
+ * **/
+void print_diagonal(int n)
+{
+	int l, f;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+			for (f = 0; f < l; f++)
+				_putchar(' ');
+			_putchar('\\');
+			if (l == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
