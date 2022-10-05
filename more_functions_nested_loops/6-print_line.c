87bc534d190441fc8035a926b5d6d412c545edd1
AgustinMeriles <5720@holbertonstudents.com>
2022-10-05 05:48:45 -0700

    a

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *  * print_line - Prints lines depending to the input "n" .
+ * *  * @n: The input int.
+ * **/
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
