8308a4ed15e0e805ffd9f75d481d1d71eb5f9e59
AgustinMeriles <5720@holbertonstudents.com>
2022-10-06 12:55:04 -0700

    a

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,11 @@
  * @s: the string
  **/
 void print_rev(char *s)
-	{
-		int i;
-
-		for (i = '(-1)'; i != s[0]; i--)
-		{
-			_putchar (s[i]);
-		}
-			_putchar ('\n');
-	}
+{
+	int len = 0;
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#include <unistd.h>
-#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
