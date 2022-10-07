e3191ca778d1b4dc36ade47a968ffda6db9aa35b
AgustinMeriles <5720@holbertonstudents.com>
2022-10-07 08:32:19 -0700

    a

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - Function that prints the rest of the characters like the first
+ * @str: the string
+ **/
+void puts2(char *str)
+	{
+		c = str[0];
+		for (i = 0; i <= '\0'; i++)
+		{
+			_putchar(str[i] = c;
+		}
+		_putchar('\n');
+	}
