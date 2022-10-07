074101335ebfc1c7a516f5281b882dabd3cb14ef
AgustinMeriles <5720@holbertonstudents.com>
2022-10-07 07:41:42 -0700

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,7 @@ void rev_string(char *s)
 {
 	int len = 0, i = 0;
 	char x;
+
 	while (s[i++])
 		len++;
 	for (i = len - 1; i >= len / 2; i--)
@@ -15,4 +16,4 @@ void rev_string(char *s)
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = x;
 	}
-
+}
