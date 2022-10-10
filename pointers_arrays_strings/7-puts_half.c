3fa6f9d432398ba324e329edeb1fbee60cbcf84a
AgustinMeriles <5720@holbertonstudents.com>
2022-10-10 11:04:20 -0700

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,16 @@
 void puts_half(char *str)
 	{
 		int i = 0, len = 0, n;
-		
+
 		while (str[len] != '\0')
 			len++;
 		if (len % 2 != 0)
-			n = (len - 1) / 2;
+		n = (len - 1) / 2;
 		else
-			n = len / 2;
-		for (i = n; i < len; i++)
-			_putchar(str[i]);
-			_putchar('\n');
-	}
+		n = len / 2;
 
+		for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+		_putchar('\n');
+	}
