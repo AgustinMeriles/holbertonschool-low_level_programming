7895c6cc7a670b64b21955b10e99cdf90f428bed
AgustinMeriles <5720@holbertonstudents.com>
2022-10-10 07:58:48 -0700

    a

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,9 @@ void puts2(char *str)
 
 		while (str[i++])
 		len++;
-		for (i = 0; i < len; i += 2)
+		for (i = 0; i < len; i++)
 		{
+			if (str[i] % 2 == 0)
 			_putchar(str[i]);
 		}
 		_putchar('\n');
