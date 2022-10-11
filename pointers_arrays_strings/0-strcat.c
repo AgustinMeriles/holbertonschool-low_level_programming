a72156897c87463c7c802efac58106b549dc3f39
AgustinMeriles <5720@holbertonstudents.com>
2022-10-11 06:34:53 -0700

    task 0-s

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: the first string;
+ * @src: the second string;
+ * Return: 0;
+ **/
+char *_strcat(char *dest, char *src)
+	{
+		int i = 0, len = 0;
+
+		while (src[i++])
+		len++;
+		for (i = 0; i <= len; i++)
+		dest += src[i];
+		printf("%s", dest);
+		return (0);
+	}
