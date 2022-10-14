e5709063b8f16d0f317d1c1104c3cc1ea4b8578c
AgustinMeriles <5720@holbertonstudents.com>
2022-10-14 09:12:11 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _print_rev_recursion - function that prints a string in reverse.
- * @s: the string;
- * Return: 0;
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: the string
+ * Return: 0
  **/
 void _print_rev_recursion(char *s)
 {
