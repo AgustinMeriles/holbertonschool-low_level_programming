102735ae0660a776a57eadf9fe9bc33ee3b48b34
AgustinMeriles <5720@holbertonstudents.com>
2022-10-14 07:12:32 -0700

    a

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: the string;
+ * Return: 0;
+ **/
+void _puts_recursion(char *s)
+{
+	printf("%s", s);
+	printf('\n');
+}
---
+++ recursion/README.md
@@ -0,0 +1,2 @@
+
+laoahaoaoaoa
