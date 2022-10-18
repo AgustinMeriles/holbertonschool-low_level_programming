0b99a8de1f3b5d259b8f96284c8aa67407cce72c
AgustinMeriles <5720@holbertonstudents.com>
2022-10-18 07:47:52 -0700

    task 0, proyect 3

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * printname - Function that prints its name
+ * @argc: the size of the array;
+ * @argv: the content of the position;
+ * Return: 0;
+ **/
+int printname(int argc, char argv)
+{
+	if (argc > 0)
+	{
+		printf ("%n", argv[0]);
+	}
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,3 @@
+#include <stdio>
+#include <stlib>
+int printname(int argc, int argv);
