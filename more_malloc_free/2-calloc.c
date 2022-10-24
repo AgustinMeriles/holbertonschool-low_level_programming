e4d0de9ff2c40d3072d93986c91850120544f95f
AgustinMeriles <5720@holbertonstudents.com>
2022-10-24 11:40:34 -0700

    task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _memset - Write a function that fills memory with a constant byte.
+ *  @s: Memory area´s pointer
+ *  @n: Bytes of memory.
+ *  @b: constant byte
+ *  Return: A pointer to the memory area s..
+ **/
+char _memset(char s, char b, unsigned int n)
+{
+	charaux = s;
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (aux);
+}
+
+/**
  * _calloc - the function,
  * @nmemb: the elements;
  * @size: the size of bytes;
@@ -18,5 +36,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+	_memset(*pun, 0, pun);
 	return (pun);
 }
