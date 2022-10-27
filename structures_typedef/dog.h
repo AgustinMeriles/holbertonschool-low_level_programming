4d1102cf2684d147efc388654b2b82b2b6ec1a3a
AgustinMeriles <5720@holbertonstudents.com>
2022-10-27 06:10:59 -0700

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 #include <stdio.h>
+/**
+ *  *struct dog - Define a new type struct dog
+ *   *@name:char
+ *    *@age:float
+ *     *@owner:char
+ **/
 struct dog
 {
 	char *name;
