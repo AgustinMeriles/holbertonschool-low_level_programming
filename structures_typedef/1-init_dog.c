9b0b43110e147aa2cf4dfc8b72b9db301d275d96
AgustinMeriles <5720@holbertonstudents.com>
2022-10-27 06:41:54 -0700

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - function that initialize a vaiable;
+ * @d: struct;
+ * @name: the name;
+ * @age: the age;
+ * @owner: the owner;
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+{
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,6 @@
-#ifndef _DOG_H_
-#define _DOG_H_
 #include <stdio.h>
-/**
- *  *struct dog - Define a new type struct dog
- *   *@name:char
- *    *@age:float
- *     *@owner:char
- **/
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-#endif
+#include <stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
