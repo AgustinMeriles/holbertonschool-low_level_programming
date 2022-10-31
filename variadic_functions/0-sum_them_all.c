6136208531855d7d42e68156c56e455754fa40f4
AgustinMeriles <5720@holbertonstudents.com>
2022-10-31 15:51:19 -0700

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function that returns the sum of alla parameters
+ * @n: a variable;
+ * Return: 0;
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+
---
+++ variadic_functions/README.md
@@ -0,0 +1,6 @@
+#ifndef VARIADIC_FUNCTIONS.H
+#define VARIADIC_FUNCTIONS.H
+#include <stdio.h>
+#include <stdlib.h>
+int sum_them_all(const unsigned int n, ...);
+#endif
