e3d0cdbd79b9f638a85ffebe282675b0dc50cf3b
AgustinMeriles <5720@holbertonstudents.com>
2022-10-31 14:01:33 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <function_pointers.h>
+/**
+ * print_name - function that will prints a name
+ * @name: the name that will function prints;
+ * @f: paramether;
+ * Return: 0;
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return (NULL);
+	if (f == NULL)
+		return (NULL);
+	(*f)(name);
+}
