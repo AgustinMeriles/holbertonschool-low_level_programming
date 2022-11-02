c8efec1eaa582590a071db1c13eb4856b02b0ccd
AgustinMeriles <5720@holbertonstudents.com>
2022-11-01 17:57:04 -0700

    task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include 
+/**
+ * print_all - function that prints anithyng
+ * @format: is a list of types of arguments passed to the function;
+ * Return: 0;
+ **/
+void print_all(const char * const format, ...)
+{
+
+}
