8999acce5f4e0869f91929a1f279fb861af9d7b0
AgustinMeriles <5720@holbertonstudents.com>
2022-11-17 10:34:04 -0800

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list
+ * @head: a pointer of head of the list
+ **/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
