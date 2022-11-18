eea4b2fe1e57e9b8ba554d7d87dbfc30f993e942
AgustinMeriles <5720@holbertonstudents.com>
2022-11-18 07:52:29 -0800

    2-add_nodeint.c

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
+	newNode->next = *head;
 	*head = newNode;
+
 	return (newNode);
 }
